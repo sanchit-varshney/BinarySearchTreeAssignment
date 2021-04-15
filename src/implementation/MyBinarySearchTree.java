881ec9fa58bc793386f0fabf8fbde4f9577cac1f
sanchit-varshney <sanchit.varshney_cs19@gla.ac.in>
2021-04-15 21:35:50 +0530

    completed the reverse inorder  method

--- src/implementation/MyBinarySearchTree.java
+++ src/implementation/MyBinarySearchTree.java
@@ -62,6 +62,11 @@ public class MyBinarySearchTree<E extends Comparable<E>> implements BinarySearch
 
     @Override
     public void reverseInOrder(Node<E> node) {
+        if(node!= null){
+            reverseInOrder(node.getRight());
+            System.out.println(node.getData()+" ");
+            reverseInOrder(node.getLeft());
+        }
 
     }
 
