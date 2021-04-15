e5179da86e0e628277ade2f1ea32658f6906cad5
sanchit-varshney <sanchit.varshney_cs19@gla.ac.in>
2021-04-15 21:32:04 +0530

    completed the post order method

--- src/implementation/MyBinarySearchTree.java
+++ src/implementation/MyBinarySearchTree.java
@@ -67,6 +67,11 @@ public class MyBinarySearchTree<E extends Comparable<E>> implements BinarySearch
 
     @Override
     public void postOrder(Node<E> node) {
+        if (node!= null){
+            postOrder(node.getLeft());
+            postOrder(node.getRight());
+            System.out.println(node.getData());
+        }
 
     }
 
