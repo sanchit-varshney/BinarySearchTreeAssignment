77bbdb8ec64e4424b9c2ce9f6ad21fb42ebf12fd
sanchit-varshney <sanchit.varshney_cs19@gla.ac.in>
2021-04-15 19:23:44 +0530

    create Constructor

--- src/implementation/Node.java
+++ src/implementation/Node.java
@@ -5,6 +5,11 @@ public class Node<E extends Comparable<E>> implements myinterface.Node<E> {
     private Node<E> left;
     private Node<E> right;
 
+    public Node(E data) {
+        this.data = data;
+        left = null;
+        right = null;
+    }
 
     //complete node class
 }
