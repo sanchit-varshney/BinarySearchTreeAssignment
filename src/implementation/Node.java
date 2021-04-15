3aa17bf43a01209ea582abe1c0e1d510a813cd59
sanchit-varshney <sanchit.varshney_cs19@gla.ac.in>
2021-04-15 19:15:43 +0530

    initialize the  private fields

--- src/implementation/Node.java
+++ src/implementation/Node.java
@@ -1,5 +1,10 @@
 package implementation;
 
 public class Node<E extends Comparable<E>> implements myinterface.Node<E> {
+    private E data;
+    private Node<E> left;
+    private Node<E> right;
+
+
     //complete node class
 }
