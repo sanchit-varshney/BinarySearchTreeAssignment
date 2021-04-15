4aa23765ee839682699807afa685e26d4b20c850
sanchit-varshney <sanchit.varshney_cs19@gla.ac.in>
2021-04-15 19:25:12 +0530

    create Getters and Setters to use private feilds

--- src/implementation/Node.java
+++ src/implementation/Node.java
@@ -11,5 +11,28 @@ public class Node<E extends Comparable<E>> implements myinterface.Node<E> {
         right = null;
     }
 
-    //complete node class
+    public E getData() {
+        return data;
+    }
+
+    public void setData(E data) {
+        this.data = data;
+    }
+
+    public Node<E> getLeft() {
+        return left;
+    }
+
+    public void setLeft(Node<E> left) {
+        this.left = left;
+    }
+
+    public Node<E> getRight() {
+        return right;
+    }
+
+    public void setRight(Node<E> right) {
+        this.right = right;
+    }
+
 }
